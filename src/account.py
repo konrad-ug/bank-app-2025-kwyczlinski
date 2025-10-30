bea60fcc1d8f98ebab06d8f5ff89b4e5720ab48b
Krzysztof Wyczlinski <k.wyczlinski.277@studms.ug.edu.pl>
2025-10-30 13:37:09 +0100

    coverage flag test

--- src/account.py
+++ src/account.py
@@ -1,15 +1,24 @@
 class Account:
     def __init__(self):
-        self.balance = 0.0
+        self.balance : float = 0.0
+        self.history : list[float] = []
 
     def incomming_transfer(self, amount : float) -> None:
         if (isinstance(amount, float) and amount >= 0):
             self.balance += amount
+            self.history.append(amount)
 
     def outgoing_transfer(self, amount : float) -> None:
         if (isinstance(amount, float) and amount >= 0 and self.balance >= amount):
             self.balance -= amount
+            self.history.append(-amount)
+
 
     def express_outgoing_transfer(self, amount : float, fee : float) -> None:
         if (isinstance(amount, float) and amount >= 0 and self.balance >= amount):
             self.balance -= amount + fee
+            self.history.append(-amount)
+            self.history.append(-fee)
+
+    def test_coverage(self):
+        pass
